1483697bf38fd1967bc7f785da0e3a5af29a244d
Karolina <k.wilczynska7@gmail.com>
2017-08-09 21:05:36 +0200

    logo component

---
+++ src/main/js/components/logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+
+    return (
+        <div className="logo">
+            <img className="logo-img" src="/src/resources/logo.png"/>
+        </div>
+
+    );
+};
+
+export default Logo;
